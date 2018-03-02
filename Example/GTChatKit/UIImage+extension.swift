e51df3d67985f963215eed803ff15d917a3b72d9
Geektree0101 <h2s1880@gmail.com>
2018-03-02 22:51:25 +0900

    GTChatKit: Fix resourse url

--- Example/GTChatKit/UIImage+extension.swift
+++ Example/GTChatKit/UIImage+extension.swift
@@ -11,11 +11,10 @@ import UIKit
 
 extension UIImage {
     static var randomURL: URL {
-        let baseURL = "https://github.com/GeekTree0101/GTChatKit/blob/master/resources"
+        let baseURL = "https://github.com/GeekTree0101/GTChatKit/blob/master/resources/"
         let gfriends = ["eumji", "eunha", "sowon", "yelin", "yuju", "sinbi"]
         let randIndex = Int(arc4random_uniform(UInt32(gfriends.count)))
         
-        return URL(string: baseURL + gfriends[randIndex] + ".jpg") ??
-            URL(string: "https://avatars1.githubusercontent.com/u/19504988?s=460&v=4")!
+        return URL(string: baseURL + gfriends[randIndex] + ".jpg")!
     }
 }
